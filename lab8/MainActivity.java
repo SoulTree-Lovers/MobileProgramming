1bf277176a01113a99e4b2724906448260e9ec1b
강승민 <rkdtmdals999@naver.com>
2023-12-06 15:46:51 +0900

    [feat] Add Lecture Code lab8, lab11

---
+++ lab8/MainActivity.java
@@ -0,0 +1,249 @@
+package ssu.btetris.singleuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import ssu.btetris.model.Tetris;
+import ssu.btetris.model.Tetris.TetrisState;
+
+public class MainActivity extends AppCompatActivity {
+    private TetrisView myTetView, peerTetView;
+    private BlockView myBlkView, peerBlkView;
+    private Button upArrowBtn, leftArrowBtn, rightArrowBtn, downArrowBtn, dropBtn, topLeftBtn, topRightBtn;
+    private Button startBtn, pauseBtn, settingBtn, modeBtn, reservedBtn;
+    //private boolean gameStarted = false;
+    private int dy = 25, dx = 15; // screen size
+
+    private TetrisModel myTetModel;
+    private Random random;
+    //private TetrisState state;
+    private char currBlk, nextBlk;
+
+    private GameState gameState = GameState.Initial;
+    private GameState savedState;
+    private enum GameState {
+        Error(-1), Initial(0), Running(1), Paused(2);
+        private final int value;
+        private GameState(int value) { this.value = value; }
+        public int value() { return value; }
+        public static GameState stateFromInteger(int value) {
+            switch (value) {
+                case -1: return Error;
+                case 0: return Initial;
+                case 1: return Running;
+                case 2: return Paused;
+                default: return null;
+            }
+        }
+    }
+    private enum GameCommand {
+        NOP(-1), Quit(0), Start(1), Pause(2), Resume(3), Update(4);
+        private final int value;
+        private GameCommand(int value) { this.value = value; }
+        public int value() { return value; }
+    }
+    int stateTransMatrix[][] = { // stateTransMatrix[currGameState][GameCommand] --> nextGameState
+            { -1, 1, -1, -1, -1 },  // [Initial][Start] --> Running
+            { 0, -1, 2, -1, 1 },    // [Running][Quit] --> Initial,
+            // [Running][Pause] --> Paused,
+            // [Running][Update] --> Running,
+            { 0, 1, -1, 1, 2 },     // [Paused][Quit] --> Initial
+            // [Paused][Start,Resume] --> Running
+            // [Paused][Update] --> Paused
+    };
+    private void executeCommand(GameCommand cmd, char key) {
+        gameState = GameState.stateFromInteger(stateTransMatrix[gameState.value()][cmd.value()]);
+        if (gameState == GameState.Error)
+            Log.d("MainActivity", "gameState is in error state!!!");
+
+        switch (cmd.value()) {
+            case 0: quitGame(); break;
+            case 1: startGame(); launchTimer(); break;
+            case 2: pauseGame(); break;
+            case 3: resumeGame(); launchTimer(); break;
+            case 4: updateModel(key); break;
+            default: Log.d("MainActivity", "unknown command!!!"); break;
+        }
+    }
+    private View.OnClickListener OnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            char key;
+            int id = v.getId();
+            GameCommand cmd = GameCommand.NOP;
+            switch (id) {
+                case R.id.startBtn: key = 'N';
+                    if (gameState == GameState.Initial) cmd = GameCommand.Start;
+                    else if (gameState == GameState.Running) cmd = GameCommand.Quit;
+                    else if (gameState == GameState.Paused) cmd = GameCommand.Quit;
+                    break;
+                case R.id.pauseBtn: key = 'P';
+                    if (gameState == GameState.Running) cmd = GameCommand.Pause;
+                    else if (gameState == GameState.Paused) cmd = GameCommand.Resume;
+                    break;
+                case R.id.upArrowBtn: key = 'w'; cmd = GameCommand.Update; break;
+                case R.id.leftArrowBtn: key = 'a'; cmd = GameCommand.Update; break;
+                case R.id.rightArrowBtn: key = 'd'; cmd = GameCommand.Update; break;
+                case R.id.downArrowBtn: key = 's'; cmd = GameCommand.Update; break;
+                case R.id.dropBtn: key = ' '; cmd = GameCommand.Update; break;
+                default: return;
+            }
+            executeCommand(cmd, key);
+        }
+    };
+    private void updateModel(char key) {
+        try {
+            TetrisState state; // model state
+            state = myTetModel.accept(key);
+            myTetView.accept(myTetModel.getScreen());
+            if (state == TetrisState.NewBlock){
+                currBlk = nextBlk;
+                nextBlk = (char) ('0' + random.nextInt(Tetris.nBlockTypes));
+                state = myTetModel.accept(currBlk);
+                myTetView.accept(myTetModel.getScreen());
+                myBlkView.accept(myTetModel.getBlock(nextBlk));
+                myBlkView.invalidate();
+                if (state == TetrisState.Finished)
+                    executeCommand(GameCommand.Quit, 'Q');
+            }
+            myTetView.invalidate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void startGame() {
+        setButtonsState(); // no argument of flag
+        startBtn.setText("Q"); // 'Q' means Quit.
+        pauseBtn.setText("P"); // 'P' means Pause.
+        Toast.makeText(MainActivity.this, "Game Started!", Toast.LENGTH_SHORT).show();
+        try {
+            TetrisState state; // model state
+            random = new Random();
+            myTetModel = new TetrisModel(dy, dx);
+            myTetView.init(dy, dx, Tetris.iScreenDw);
+            myBlkView.init(Tetris.iScreenDw);
+            currBlk = (char) ('0' + random.nextInt(Tetris.nBlockTypes));
+            nextBlk = (char) ('0' + random.nextInt(Tetris.nBlockTypes));
+            state = myTetModel.accept(currBlk);
+            myTetView.accept(myTetModel.getScreen());
+            myBlkView.accept(myTetModel.getBlock(nextBlk));
+            myTetView.invalidate();
+            myBlkView.invalidate();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void quitGame() {
+        setButtonsState(); // no argument of flag
+        startBtn.setText("N"); // 'N' means New Game.
+        Toast.makeText(MainActivity.this, "Game Over!", Toast.LENGTH_SHORT).show();
+    }
+    private void pauseGame() {
+        setButtonsState();
+        pauseBtn.setText("R");
+        Toast.makeText(MainActivity.this, "Game Paused!", Toast.LENGTH_SHORT).show();
+    }
+    private void resumeGame() {
+        setButtonsState();
+        pauseBtn.setText("P");
+        Toast.makeText(MainActivity.this, "Game Resumed!", Toast.LENGTH_SHORT).show();
+    }
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable runnableTimer = new Runnable() {
+        @Override
+        public void run() {
+            if (gameState == GameState.Running) {
+                updateModel('s');
+                Log.d("MainActivity", "ondraws="+myTetView.ondraw_calls);
+                handler.postDelayed(this, 1000);
+            }
+        }
+    };
+    private void launchTimer() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) // P means 'Android Pie', aka Android 9.0
+            handler.postDelayed(runnableTimer, 0, 1000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        savedState = gameState;
+        if (gameState == GameState.Running)
+            executeCommand(GameCommand.Pause, 'P');
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (savedState == GameState.Running) {
+            savedState = GameState.Paused;
+            executeCommand(GameCommand.Resume, 'R');
+        }
+    }
+    boolean buttonState[][] = { // buttonState[currGameState][btnID] --> enable/disable
+            { true, false, false }, // [Initial][startBtn] --> enabled
+            { true, true, true },   // [Running][anyBtn] --> enabled
+            { true, true, false },  // [Paused][startBtn,resumeBtn] --> enabled
+    };
+    private void setButtonsState() {
+        boolean startFlag = buttonState[gameState.value()][0];
+        boolean pauseFlag = buttonState[gameState.value()][1];
+        boolean defaultFlag = buttonState[gameState.value()][2];
+        startBtn.setEnabled(startFlag);
+        pauseBtn.setEnabled(pauseFlag);
+        settingBtn.setEnabled(false);
+        modeBtn.setEnabled(false);
+        reservedBtn.setEnabled(false);
+
+        upArrowBtn.setEnabled(defaultFlag);
+        leftArrowBtn.setEnabled(defaultFlag);
+        rightArrowBtn.setEnabled(defaultFlag);
+        downArrowBtn.setEnabled(defaultFlag);
+        dropBtn.setEnabled(defaultFlag);
+        topLeftBtn.setEnabled(false); // always disabled
+        topRightBtn.setEnabled(false); // always disabled
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myTetView = (TetrisView) findViewById(R.id.myTetrisView);
+        peerTetView = (TetrisView) findViewById(R.id.peerTetrisView);
+        myBlkView = (BlockView) findViewById(R.id.myBlockView);
+        peerBlkView = (BlockView) findViewById(R.id.peerBlockView);
+        startBtn = (Button) findViewById(R.id.startBtn);
+        pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        settingBtn = (Button) findViewById(R.id.settingBtn);
+        modeBtn = (Button) findViewById(R.id.modeBtn);
+        reservedBtn = (Button) findViewById(R.id.reservedBtn);
+        upArrowBtn = (Button) findViewById(R.id.upArrowBtn);
+        leftArrowBtn = (Button) findViewById(R.id.leftArrowBtn);
+        rightArrowBtn = (Button) findViewById(R.id.rightArrowBtn);
+        downArrowBtn = (Button) findViewById(R.id.downArrowBtn);
+        dropBtn = (Button) findViewById(R.id.dropBtn);
+        topLeftBtn = (Button) findViewById(R.id.topLeftBtn);
+        topRightBtn = (Button) findViewById(R.id.topRightBtn);
+
+        startBtn.setOnClickListener(OnClickListener);
+        pauseBtn.setOnClickListener(OnClickListener);
+        settingBtn.setOnClickListener(OnClickListener);
+        modeBtn.setOnClickListener(OnClickListener);
+        reservedBtn.setOnClickListener(OnClickListener);
+
+        upArrowBtn.setOnClickListener(OnClickListener);
+        leftArrowBtn.setOnClickListener(OnClickListener);
+        rightArrowBtn.setOnClickListener(OnClickListener);
+        downArrowBtn.setOnClickListener(OnClickListener);
+        dropBtn.setOnClickListener(OnClickListener);
+
+        setButtonsState(); // no argument of flag
+    }
+}
